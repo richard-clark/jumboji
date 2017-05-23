71c4397b45765415fb3549f9276c73e331c6af7d
Richard Clark <rclark@rjmetrics.com>
2017-05-23 00:38:52 -0400

    Recompiled.

--- webpack.config.js
+++ webpack.config.js
@@ -3,68 +3,72 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
-const isProduction = process.env.NODE_ENV === "production";
-console.log(isProduction);
+module.exports = function(env) {
+  if (env !== "dev" && env !== "prod") {
+    throw new Error("You must specify an environment (dev/prod) with '--env prod'");
+  }
+  const isProduction = env === "prod";
 
-module.exports = {
-  devServer: {
-    contentBase: path.join(__dirname, "docs"),
-    compress: true,
-    port: 9000,
-    hot: true,
-    historyApiFallback: {
-      index: "/"
-    }
-  },
+  return {
+    devServer: {
+      contentBase: path.join(__dirname, "docs"),
+      compress: true,
+      port: 9000,
+      hot: true,
+      historyApiFallback: {
+        index: "/"
+      }
+    },
 
-  entry: "./src/index.js",
+    entry: "./src/index.js",
 
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }, {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ["es2015", "stage-0"],
-            plugins: ["transform-regenerator"]
-          }
-        }
-      }, {
-        test: /\.(json|svg)$/,
-        loader: "file-loader"
-      }, {
-        test: /worker\.js$/,
-        // include: paths.appSrc,
-        use: [
-          { loader: 'worker-loader' },
-          { loader: 'babel-loader',
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        }, {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
             options: {
               presets: ["es2015", "stage-0"],
               plugins: ["transform-regenerator"]
             }
           }
-        ]
-      },
+        }, {
+          test: /\.(json|svg)$/,
+          loader: "file-loader"
+        }, {
+          test: /worker\.js$/,
+          // include: paths.appSrc,
+          use: [
+            { loader: 'worker-loader' },
+            { loader: 'babel-loader',
+              options: {
+                presets: ["es2015", "stage-0"],
+                plugins: ["transform-regenerator"]
+              }
+            }
+          ]
+        },
+      ]
+    },
+
+    output: {
+      filename: "bundle.js",
+      path: __dirname + "/docs",
+      publicPath: isProduction ? "/jumboji/" : "/"
+    },
+
+    plugins: [
+      new FaviconsWebpackPlugin("./favicon.png"),
+      new HtmlWebpackPlugin({
+        baseUrl: isProduction ? "/jumboji/" : "/",
+        title: "Jumboji 🕺🏽"
+      }),
+      new webpack.HotModuleReplacementPlugin()
     ]
-  },
-
-  output: {
-    filename: "bundle.js",
-    path: __dirname + "/docs",
-    publicPath: isProduction ? "/jumboji/" : "/"
-  },
-
-  plugins: [
-    new FaviconsWebpackPlugin("./favicon.png"),
-    new HtmlWebpackPlugin({
-      baseUrl: isProduction ? "/jumboji/" : "/",
-      title: "Jumboji 🕺🏽"
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ]
-};
+  };
+}
